f375d795458d301f886302a88828d401678f58d4
Zan Marolt <zan.marolt@3fs.si>
2019-05-05 09:38:50 +0200

    Add code coverage test

---
+++ testing-demos/code-coverage.spec.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer');
+const pti = require('puppeteer-to-istanbul');
+
+const waitAndGetText = async (page, selector) => {
+    const element = await page.waitFor(selector);
+    return await page.evaluate(element => element.textContent.trim(), element);
+};
+
+describe('Code-coverage-e2e', () => {
+  beforeAll(async () => {
+    browser = await puppeteer.launch({headless: false});
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  it('Generate istanbul code coverage report', async () => {
+    const page = await browser.newPage();
+
+    await page.coverage.startJSCoverage();
+
+    await page.goto('http://localhost:3002/');
+
+    const numClients = await waitAndGetText(page, '#num-clients:not(:empty)');
+    expect(numClients).toBe('1');
+
+    const message = 'Hello, Google IO!';
+    await page.type('#message', message);
+    await page.click('#send');
+
+    const displayedText = await waitAndGetText(page, '.my-message');
+    expect(displayedText).toBe(message);
+
+    const jsCoverage = await page.coverage.stopJSCoverage();
+    pti.write(jsCoverage);
+
+    await page.close();
+  });
+})
